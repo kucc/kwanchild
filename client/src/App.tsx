406e84b881b8cfe56ad7bd2298ae2884e27bf228
deltaori0 <deltaori0@korea.ac.kr>
2020-11-07 06:08:53 +0900

    add: problem4

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -10,6 +10,7 @@ import Main from "./view/main";
 import Problem1 from "./view/problem1";
 import Problem2 from "./view/problem2";
 import Problem3 from "./view/problem3";
+import Problem4 from "./view/problem4";
 import Rank from "./view/rank";
 
 function App() {
@@ -20,6 +21,7 @@ function App() {
         <Route path="/problem1" exact component={Problem1} />
         <Route path="/problem2" exact component={Problem2} />
         <Route path="/problem3" exact component={Problem3} />
+        <Route path="/problem4" exact component={Problem4} />
         <Route path="/rank" exact component={Rank} />
         <Redirect from="*" to="/" />
       </Switch>
---
+++ client/src/view/problem4/index.tsx
@@ -0,0 +1,54 @@
+import React, { useState, useCallback, useEffect } from "react";
+import { Redirect } from "react-router-dom";
+import { useHistory } from "react-router-dom";
+import { STATIC_URL } from "../../constant";
+
+import * as S from "./styles";
+
+const Problem4: React.FC = () => {
+  const [userAnswer, setUserAnswer] = useState("");
+  const answer = "kucc";
+  let history = useHistory();
+
+  const checkAnswer = useCallback(() => {
+    if (answer === userAnswer) {
+      console.log("correct");
+      history.push("/problem5");
+    } else {
+      alert("틀렸습니다!");
+    }
+  }, [userAnswer]);
+
+  return (
+    <S.Problem4>
+      <S.ProblemLayout>
+        <S.Title>문제 4</S.Title>
+        <S.Content>
+          나를 찾아 봐!
+          <S.HintContainer>
+            I l I l I l I l I I I l I I I <br />
+            I I l l I l I l I l l l I l l <br />
+            I l l l I l I l I l l l I l l <br />
+            I I l l I l I l I l l l I l l <br />
+            I l I l I I I l I I I l I I I <br />
+          </S.HintContainer>
+        </S.Content>
+        <S.AnswerContainer>
+          <S.InputBox
+            placeholder="정답을 입력하세요."
+            onChange={(e) => {
+              setUserAnswer(e.target.value);
+              // console.log(e.target.value);
+            }}
+            onKeyDown={(e) => {
+              if (e.key === "Enter") checkAnswer();
+            }}
+          ></S.InputBox>
+          <S.SubmitButton onClick={checkAnswer}>입력</S.SubmitButton>
+        </S.AnswerContainer>
+      </S.ProblemLayout>
+    </S.Problem4>
+  );
+};
+
+export default Problem4;
