85d17fec1b82f270f9eb015f806b9698e140680d
deltaori0 <deltaori0@korea.ac.kr>
2020-11-06 20:42:24 +0900

    feat: initial setting

---
+++ src/constant.ts
@@ -0,0 +1,12 @@
+export const BREAKPOINT = 640;
+
+export const STATIC_URL = {
+  BACKGROUND: `https://user-images.githubusercontent.com/50616334/98361083-3c97ba00-206e-11eb-82f8-9565426b64b4.png`,
+};
+
+export const THEME_COLOR = {
+  PRIMARY: "#C2201F",
+  SECONDARY: "#F19296",
+  GRAY: "#F1F1F1",
+  GRAYER: "#EDEDED",
+};
