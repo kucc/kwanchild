acb2a9cc726c6a431ebb769ce1c5b8b3f82d0ad1
limjisuu <dlawltn1999@naver.com>
2020-11-07 00:44:58 +0900

    잠깐

--- client/src/view/main/index.tsx
+++ client/src/view/main/index.tsx
@@ -2,15 +2,20 @@ import React from "react";
 import { STATIC_URL } from "../../constant";
 import * as S from "./styles";
 import { useCallback, useState } from "react";
-
-// const [isStart, setStart] = useState(false);
+import { useHistory } from "react-router-dom";
 
 const Main: React.FC = () => {
-  let isStart = false;
+  const [isStart, setStart] = useState(false);
+  let history = useHistory();
+  // let isStart = false;
   const confirmName = useCallback(async () => {
     // setStart(!isStart);
-    isStart = true;
+    setStart(true);
     console.log(isStart);
+  }, [isStart]);
+
+  const confirmStart = useCallback(async () => {
+    history.push("/problem1");
   }, []);
 
   return (
@@ -18,7 +23,7 @@ const Main: React.FC = () => {
       <S.Title>문제적 KUCC</S.Title>
       <S.ButtonContainer>
         {isStart ? (
-          <S.Button>시작하기</S.Button>
+          <S.Button onClick={confirmStart}>시작하기</S.Button>
         ) : (
           <S.NameContainer>
             <S.NameInput placeholder="닉네임을 입력해 주세요"></S.NameInput>
